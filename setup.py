82495f08e10f3015e60832a77f7903aaded6992e
Edison Rocha <edisoncoder@gmail.com>
2018-11-10 01:32:40 -0200

    Inicializando

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FaceRecognRocha',
+    version='1.0',
+    packages=['facerecognrocha', 'facerecognrocha.tests'],
+    url='',
+    license='',
+    author='Edison Rocha',
+    author_email='edisoncoder@gmail.com',
+    description='Reconhecimento facial com comando via mensageria'
+)
